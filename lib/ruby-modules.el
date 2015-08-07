8437c4e146b8a6104fcfb68fff7e0555e692bda9
Patrick Brown <pbrown@fiksu.com>
2015-08-07 16:23:21 -0400

    don't use rainbow delims, don't need to spec bash

--- lib/ruby-modules.el
+++ lib/ruby-modules.el
@@ -7,14 +7,8 @@
     :config
     (add-hook 'after-init-hook 'inf-ruby-switch-setup)
     (rspec-mode)
-    (defadvice rspec-compile (around rspec-compile-around)
-      "Use BASH shell for running the specs because of ZSH issues."
-      (let ((shell-file-name "/bin/bash"))
-        ad-do-it))
     (ad-activate 'rspec-compile))
   (use-package rvm)
-  (use-package rainbow-delimiters
-    :config (rainbow-delimiters-mode-enable))
   (whitespace-mode t))
 
 (add-hook' ruby-mode-hook 'ruby-modes)
