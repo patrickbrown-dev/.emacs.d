086102c953ecd7f3e118ba1f576b6d32daea7d5d
Patrick Brown <patrick.arthur.brown@gmail.com>
2015-08-08 16:36:35 -0400

    title change, remove user vars

--- init.el
+++ init.el
@@ -36,21 +36,3 @@
 (if (display-graphic-p) (server-start))
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-safe-themes
-   (quote
-    ("83e584d74b0faea99a414a06dae12f11cd3176fdd4eba6674422539951bcfaa8" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
