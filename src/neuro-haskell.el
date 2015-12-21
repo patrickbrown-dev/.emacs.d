9d59f5586173b8b5c1bf9fdba82ff2f31d27282c
Patrick Brown <patrick.arthur.brown@gmail.com>
2015-12-20 23:05:45 -0800

    Update haskell config

--- src/neuro-haskell.el
+++ src/neuro-haskell.el
@@ -1,9 +1,12 @@
 ;;; neuro-haskell.el --- haskell configuration.
 ;;; Commentary:
 ;;; Code:
-(add-hook' haskell-mode-hook 
+(add-hook' haskell-mode-hook
            (lambda ()
              (use-package haskell-mode)
+             (haskell-indent-mode t)
+             (haskell-indentation-mode t)
+             (add-to-list 'company-backends 'company-ghc)
              (whitespace-mode t)))
 
 (provide 'neuro-haskell)
