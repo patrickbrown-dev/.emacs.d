97383d1fce4c30e55384d201b6c453e3aa9fceab
Patrick Brown <patrick.arthur.brown@gmail.com>
2016-01-09 17:14:30 -0800

    Move customization to separate file out of repo

--- init.el
+++ init.el
@@ -32,6 +32,10 @@
 (if (file-exists-p "~/.emacs.d/secrets.el")
     (load "~/.emacs.d/secrets"))
 
+;; Customization file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 (require 'neuro-defaults)
 (if (display-graphic-p)
     (require 'neuro-gui)
@@ -44,21 +48,3 @@
 (require 'neuro-rust)
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-safe-themes
-   (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
