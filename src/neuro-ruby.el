08560d5954224ec1d6c2a76f98f6485c21971dee
Patrick Brown <patrick.arthur.brown@gmail.com>
2016-01-22 01:41:14 -0800

    Create ruby rspec outline interactive command

--- src/neuro-ruby.el
+++ src/neuro-ruby.el
@@ -1,14 +1,25 @@
 ;;; neuro-ruby.el --- ruby configuration.
+;;;
 ;;; Commentary:
+;;; Copyright (C) 2016 by Patrick Arthur Brown
+;;;
 ;;; Code:
-(add-hook' ruby-mode-hook 
-           (lambda ()
-             (use-package rspec-mode
-               :config
-               (add-hook 'after-init-hook 'inf-ruby-switch-setup)
-               (rspec-mode))
-             (use-package rvm)
-             (whitespace-mode t)))
+(add-hook'
+ ruby-mode-hook
+ (lambda ()
+   (use-package rspec-mode
+     :config
+     (add-hook 'after-init-hook 'inf-ruby-switch-setup)
+     (rspec-mode))
+
+   (use-package rvm)
+
+   (whitespace-mode t)
+
+   (defun rspec-outline ()
+     "Opens a buffer with rspec outline."
+     (interactive)
+     (multi-occur (list (current-buffer)) "^\s+\\(it\\\|describe\\\|context\\)"))))
 
 (provide 'neuro-ruby)
 ;;; neuro-ruby.el ends here
