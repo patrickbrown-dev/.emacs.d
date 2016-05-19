fb11142e511f9422285b3460c9c71be3bd0e1a36
Patrick Arthur Brown <pab@hivemind.us.org>
2016-05-19 12:23:17 -0700

    Next/previous buffer keybinds

--- lib/pb-keybinds.el
+++ lib/pb-keybinds.el
@@ -8,6 +8,11 @@
 ;; make backspace key work properly when running in a terminal
 (global-set-key (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "C-M-h") 'backward-kill-word)
+
+;; Next/previous buffers
+(global-set-key (kbd "C-c n") 'next-buffer)
+(global-set-key (kbd "C-c p") 'previous-buffer)
+
 (global-set-key "\C-s" 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "<f6>") 'ivy-resume)
