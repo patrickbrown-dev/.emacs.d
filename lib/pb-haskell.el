523416f7d4168d6b3e3adb8baf5cc0a89427e041
Patrick Arthur Brown <pab@hivemind.us.org>
2016-06-02 21:39:13 -0700

    Use intero.el for haskell

--- lib/pb-haskell.el
+++ lib/pb-haskell.el
@@ -4,9 +4,7 @@
 (add-hook' haskell-mode-hook
            (lambda ()
              (use-package haskell-mode)
-             (haskell-indent-mode t)
-             (haskell-indentation-mode t)
-             (add-to-list 'company-backends 'company-ghc)
+             (require 'intero)
              (nlinum-mode t)
              (whitespace-mode t)))
 
