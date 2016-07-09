0afe2698626913a5dbd4302d759838a95bc9753b
Patrick Arthur Brown <pab@hivemind.us.org>
2016-07-08 20:30:24 -0700

    Use https

--- init.el
+++ init.el
@@ -9,9 +9,9 @@
 ;;; Code:
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (when (< emacs-major-version 24)
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 
