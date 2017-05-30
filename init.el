6a12721487eef38a56c42cf292fada9ad7e13389
Patrick Arthur Brown <pat@pab.io>
2017-05-30 12:53:43 -0700

    Remove code for emacs < 24 compatibility

--- init.el
+++ init.el
@@ -10,9 +10,6 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
-(when (< emacs-major-version 24)
-  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
-
 (package-initialize)
 
 (setq ispell-program-name "/usr/local/bin/ispell")
